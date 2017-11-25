329684ff68afb99697ab0073bfc876f1ffe364cb
anilkumarsandrapati <anil.sandrapati@walkingtree.tech>
2017-11-25 15:16:18 +0530

    Aded content-security-policy for helmet

--- index.js
+++ index.js
@@ -12,8 +12,30 @@ const express = require('express');
  */
 const helmet = require('helmet');
 
+/**
+ * Requiring express csp helmet
+ * This is an Express extension which allows you to set the content-security-policy for your Express Application.
+ * https://www.npmjs.com/package/express-csp
+ */
+const csp = require('express-csp-header');
+
+const cspMiddleware = csp({
+    policies: {
+        'default-src': [csp.NONE],
+        'script-src': [csp.NONCE],
+        'style-src': [csp.NONCE],
+        'img-src': [csp.SELF],
+        'font-src': [csp.NONCE, 'fonts.gstatic.com'],
+        'object-src': [csp.NONE],
+        'block-all-mixed-content': true,
+        'frame-ancestors': [csp.NONE]
+    }
+});
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(cspMiddleware);
 
 // Sets "Strict-Transport-Security: max-age=5184000; includeSubDomains".
 var sixtyDaysInSeconds = 5184000
@@ -32,7 +54,17 @@ app.use(helmet({
 
 //express route
 app.get('/', (req, res) => {
-    res.send(`<h1>Hello World</h1>`);
+    res.send(`
+    <h1>Hello World</h1>
+    <style nonce=${req.nonce}>
+    .blue {background:cornflowerblue;color:white}
+    </style>
+    <p class="blue"> This should have a blue background because of the loaded styles </p>
+    <style>
+    .red {background:maroon;color:white}
+    </style>
+    <p class="red"> This should not  have a red background, tgue styles are not loaded because of the missings nonce </p>
+    `);
 });
 
 //listening express
