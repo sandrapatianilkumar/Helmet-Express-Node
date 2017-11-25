3f2fe5845b3196ccf9c55bbe50c705ddf3376f00
anilkumarsandrapati <anil.sandrapati@walkingtree.tech>
2017-11-25 14:51:09 +0530

    Helmet configurations - dnsPrefetchControl, frameguard, hsts

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-// requiring express 
+/**
+ * requiring express 
+ * Express supports robust routing catching 
+ * https://www.npmjs.com/package/express
+ */
 const express = require('express');
+
+/**
+ * Requiring Helmet
+ * Helmet helps you secure your Express apps by setting various HTTP headers.
+ * https://helmetjs.github.io/
+ */
+const helmet = require('helmet');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+// Sets "Strict-Transport-Security: max-age=5184000; includeSubDomains".
+var sixtyDaysInSeconds = 5184000
+
+app.use(helmet({
+    dnsPrefetchControl: {
+        allow: true
+    },
+    frameguard: {
+        action: 'deny'
+    },
+    hsts: {
+        maxAge: sixtyDaysInSeconds
+    }
+}));
 
 //express route
 app.get('/', (req, res) => {
